3aaca4fcc3fa1fc02596f234cd2607b9f31622d5
Jamie Hardt <jhardt@soundepartment.com>
2014-06-28 12:19:49 -0700

    Initial Commit

---
+++ JHWaveform2/JHWaveform2.h
@@ -0,0 +1,19 @@
+//
+//  JHWaveform2.h
+//  JHWaveform2
+//
+//  Created by Jamie Hardt on 6/28/14.
+//  Copyright (c) 2014 Jamie Hardt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JHWaveform2.
+FOUNDATION_EXPORT double JHWaveform2VersionNumber;
+
+//! Project version string for JHWaveform2.
+FOUNDATION_EXPORT const unsigned char JHWaveform2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHWaveform2/PublicHeader.h>
+
+
