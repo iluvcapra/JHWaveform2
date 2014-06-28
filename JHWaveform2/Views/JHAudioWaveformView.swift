169bd0fa40efc29b34609437ccd678bcfe99bde5
Jamie Hardt <jhardt@soundepartment.com>
2014-06-28 12:21:10 -0700

    New class - waveformView

---
+++ JHWaveform2/Views/JHAudioWaveformView.swift
@@ -0,0 +1,24 @@
+//
+//  JHAudioWaveformView.swift
+//  JHWaveform2
+//
+//  Created by Jamie Hardt on 6/28/14.
+//  Copyright (c) 2014 Jamie Hardt. All rights reserved.
+//
+
+import Cocoa
+
+class JHAudioWaveformView: NSView {
+
+    init(frame: NSRect) {
+        super.init(frame: frame)
+        // Initialization code here.
+    }
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        
+    }
+    
+}
