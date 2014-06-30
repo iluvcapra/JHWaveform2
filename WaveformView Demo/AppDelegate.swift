ccff0176e94594d09a3b7e8e4d2efa6c8e2ec81f
Jamie Hardt <jhardt@soundepartment.com>
2014-06-29 21:48:26 -0700

    Adding a demo app

---
+++ WaveformView Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WaveformView Demo
+//
+//  Created by Jamie Hardt on 6/29/14.
+//  Copyright (c) 2014 Jamie Hardt. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WaveformView Demo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  WaveformView Demo
+//
+//  Created by Jamie Hardt on 6/29/14.
+//  Copyright (c) 2014 Jamie Hardt. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
