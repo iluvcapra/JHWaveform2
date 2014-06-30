85eaeaf3d701c432e9c7f6f4bf0cfb07cce802de
Jamie Hardt <jhardt@soundepartment.com>
2014-06-29 19:22:32 -0700

    Added new class for file reading

---
+++ JHWaveform2Tests/JHTransformingFrameProvider_Files_Test.swift
@@ -0,0 +1,25 @@
+//
+//  JHTransformingFrameProvider_Files_Test.swift
+//  JHWaveform2
+//
+//  Created by Jamie Hardt on 6/29/14.
+//  Copyright (c) 2014 Jamie Hardt. All rights reserved.
+//
+
+import XCTest
+
+class JHTransformingFrameProvider_Files_Test: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+
+    1
+}
