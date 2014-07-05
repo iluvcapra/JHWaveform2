a09263771bc00d99de79799a24dc623fd7d31e1e
Jamie Hardt <jhardt@soundepartment.com>
2014-07-05 13:08:18 -0700

    Audio waveform control

---
+++ JHWaveform2/JHAudioWavefromControl.swift
@@ -0,0 +1,24 @@
+//
+//  JHAudioWavefromControl.swift
+//  JHWaveform2
+//
+//  Created by Jamie Hardt on 7/5/14.
+//  Copyright (c) 2014 Jamie Hardt. All rights reserved.
+//
+
+import Cocoa
+
+class JHAudioWavefromControl: NSControl {
+
+    init(frame: NSRect) {
+        super.init(frame: frame)
+        // Initialization code here.
+    }
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
