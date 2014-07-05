4dec46489a1c562c67ab3ca65a55c87fcc949384
Jamie Hardt <jhardt@soundepartment.com>
2014-07-05 13:24:40 -0700

    Multicolor channel size

--- JHWaveform2/JHMultichannelWaveformView.swift
+++ JHWaveform2/JHMultichannelWaveformView.swift
@@ -45,13 +45,16 @@ class JHMultichannelWaveformView: NSView {
             
             self.channelViews = Array(0..fp.channelCount).map {
                 var thisframe = NSRect(x: 0.0,
-                    y: height * CGFloat($0),
+                    y: height * CGFloat((fp.channelCount - 1) - $0),
                     width: self.bounds.width,
                     height: height)
                 
                 var channelView = JHAudioWaveformView(frame: thisframe)
                 channelView.channel = $0
                 channelView.frameProvider = fp
+                var color = self.channelColorCarousel[ $0 % self.channelColorCarousel.count ]
+                channelView.strokeColor = color
+                channelView.fillColor = color.highlightWithLevel(0.5)
                 
                 return channelView
             }
@@ -60,6 +63,14 @@ class JHMultichannelWaveformView: NSView {
     }
     
     var backgroundColor: NSColor = NSColor.controlBackgroundColor()
+    
+    let channelColorCarousel: Array<NSColor> = [
+        NSColor.redColor(),
+        NSColor.blueColor(),
+        NSColor.orangeColor(),
+        NSColor.greenColor(),
+        NSColor.purpleColor(),
+        NSColor.brownColor()]
     
     init(frame: NSRect) {
         super.init(frame: frame)
